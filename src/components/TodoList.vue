168870b55e18b0767ed309d8e1991165c631745d
yeji.jo <choyeji1591@gmail.com>
2023-03-13 15:46:52 +0900

    fix : checkBox 버튼 클릭

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div v-for="(todo, index) in todos" :key="todo.id" class="bg-purple-50 mb-2">
     <div class="flex ml-8 mr-8 h-3rem bg-white font-bold text-purple-400 border-round align-items-center  justify-content-between">
       <div>
-        <check-box class="form-check-input" :input-id="todo.id" name="id" v-model="todo.completed" :binary="todo.completed"/>
+        <check-box class="form-check-input" :input-id="todo.id" name="id" v-model="todo.completed" :binary="true"/>
         <label class="form-check-label" :for="todo.id" :class="{todo: todo.completed}">{{todo.subject}} {{ todo.completed}}</label>
       </div>
       <click-button class="flex form-check-button mr-2" label="Delete" @click="deleteTodo(index)"></click-button>
@@ -13,9 +13,9 @@
 <script setup>
 import {defineProps,defineEmits } from 'vue';
 
-defineProps(['todos']);
-
 const emit = defineEmits(['toggle-todo','delete-todo']);
+
+defineProps(['todos']);
 
 const deleteTodo = (index) =>{
   emit('delete-todo',index);
